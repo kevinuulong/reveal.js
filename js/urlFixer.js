9eae58cead4c90f4299442982e3a04806b668c29
Kevin Long <kevin.uu.long@gmail.com>
2021-03-02 20:47:55 -0500

    Actually fix the URL's

--- js/urlFixer.js
+++ js/urlFixer.js
@@ -1,6 +1,8 @@
-// This fixes URL's that look like: sites.kevinuulong.com/s/github#/ => sites.kevinuulong.com/s/github/#/
-let url = window.location.href;
-if (url.match(/(\w)(#)(.*)/) != null) {
-    url = url.replace(/(\w)(#)(.*)/, "$1/#$3");
-    window.location.href = url;
+// This fixes URL's that look like: slides.kevinuulong.com/s/github#/ => slides.kevinuulong.com/s/github/#/
+function fixUrl() {
+    let url = window.location.href;
+    if (url.match(/(\w)(#)(.*)/) != null) {
+        url = url.replace(/(\w)(#)(.*)/, "$1/#$3");
+        window.location.href = url;
+    }
 }
